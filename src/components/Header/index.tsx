6c80f44ca2f84d4ed554816cf09d35b6db75ba92
Kayo Elias <46985165+OyakSaile@users.noreply.github.com>
2022-07-21 20:26:03 -0300

    sound to yellow button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
 import { HeaderPage } from "./styles";
+import { useState, useRef, useEffect } from "react";
+import quack from "./sounds/quack.mp3";
+import useSound from "use-sound";
 export function Header() {
+  const [play] = useSound(quack);
   return (
     <HeaderPage>
       <span className="red"></span>
-      <span className="yellow"></span>
+      <span
+        className="yellow"
+        onClick={() => {
+          play()
+        }}
+      ></span>
       <span className="green"></span>
     </HeaderPage>
   );
