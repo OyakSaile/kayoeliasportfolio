e99cc5d2d9fd762bdfa1c30a9819793b2adebf54
Kayo Elias <46985165+OyakSaile@users.noreply.github.com>
2022-07-21 20:48:38 -0300

    green button animation

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
+import ReactConfetti from "react-confetti";
 import { Header } from "../../components/Header";
 import { Container } from "./styles";
 export function HomePage() {
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  const [conffetiAnimation, setConffetiAnimation] = useState(false);
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  function getWindowSize() {
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
+  }
+
+  function handleStartAnimation() {
+    setConffetiAnimation(!conffetiAnimation);
+  }
+
+  console.log(windowSize);
   return (
     <Container>
-      <Header />
+      <Header handleStartAnimation={handleStartAnimation} />
+      {conffetiAnimation && (
+        <ReactConfetti
+          width={windowSize.innerWidth}
+          height={windowSize.innerHeight}
+        />
+      )}
     </Container>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,12 @@ import { HeaderPage } from "./styles";
 import { useState, useRef, useEffect } from "react";
 import quack from "./sounds/quack.mp3";
 import useSound from "use-sound";
-export function Header() {
+import Confetti from "react-confetti";
+
+interface IHeaderProps {
+  handleStartAnimation: () => void;
+}
+export function Header({ handleStartAnimation }: IHeaderProps) {
   const [play] = useSound(quack);
   return (
     <HeaderPage>
@@ -10,10 +15,15 @@ export function Header() {
       <span
         className="yellow"
         onClick={() => {
-          play()
+          play();
         }}
       ></span>
-      <span className="green"></span>
+      <span
+        className="green"
+        onClick={() => {
+          handleStartAnimation();
+        }}
+      ></span>
     </HeaderPage>
   );
 }
